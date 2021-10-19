b839a1b3de07df27b5027c77216a3c12a94490e7
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-18 19:06:36 -0700

    add ttitle to video

--- script.js
+++ script.js
@@ -7,19 +7,23 @@
 
 function preview_video(element) {                               // Function to start playing video on mute
     var video_object = document.getElementById("main_video");   // get an instance of the video object
-    video_object.autoplay = true;                               // turn on autoplay
     video_object.muted = true;                                  // turn on mute
-    video_object.load();                                        // reload the video
+    video_object.play();
 }
 
 function pause_video(element) {                                 // Function to stop playing video on mute
     var video_object = document.getElementById("main_video");   // get an instance of the video project
-    video_object.autoplay = false;                              // turn off autoplay
-    video_object.muted = false;                                 // turn off mute
-    video_object.load();                                        // reload the video
+    video_object.muted = false;
+    video_object.pause();
+}
+
+function get_main_video_title() {                               // Function that returns the title of the video
+    var video_object = document.getElementById("main_video");
+    return video_object.title;
 }
 
 //// MAIN EXECUTABLE SECTION ///////////////////////////////
 
 console.log("page loaded...");
 
+document.getElementById("main_video_title").innerText = get_main_video_title();  // Place the title of the main video
