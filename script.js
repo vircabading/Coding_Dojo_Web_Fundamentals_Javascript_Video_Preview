39614287280950c6b1ca4fa56eb577eec5d37ad1
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-18 18:38:26 -0700

    add autoplay with mute on hover

--- script.js
+++ script.js
@@ -5,7 +5,19 @@
 
 //// FUNCTIONS /////////////////////////////////////////////
 
+function preview_video(element) {                               // Function to start playing video on mute
+    var video_object = document.getElementById("main_video");   // get an instance of the video object
+    video_object.autoplay = true;                               // turn on autoplay
+    video_object.muted = true;                                  // turn on mute
+    video_object.load();                                        // reload the video
+}
 
+function pause_video(element) {                                 // Function to stop playing video on mute
+    var video_object = document.getElementById("main_video");   // get an instance of the video project
+    video_object.autoplay = false;                              // turn off autoplay
+    video_object.muted = false;                                 // turn off mute
+    video_object.load();                                        // reload the video
+}
 
 //// MAIN EXECUTABLE SECTION ///////////////////////////////
 
